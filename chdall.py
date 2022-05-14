9bb352b947fde01d87dd34eebe0467ece972544d
BertrandHustle <scott.greenberg926@gmail.com>
2022-05-14 13:53:36 -0400

    local testing works

--- chdall.py
+++ chdall.py
@@ -12,26 +12,44 @@ def find_pattern(pattern, path):
                 return os.path.join(root, name)
 
 
-if not os.path.exists('chdman.exe'):
-    raise FileNotFoundError('chdman.exe not found!')
+def convert_chds():
+    for filepath in os.listdir() :
 
-for filepath in os.listdir():
-    bin = find_pattern('*.bin', filepath)
-    cue = find_pattern('*.cue', filepath)
-    bin_game_name = bin.split('.cue')[0]
-    cue_game_name = cue.split('.cue')[0]
-    # error handling
-    if not cue or not bin:
-        print(f'.cue and .bin not found in {filepath}!')
-        continue
-    if bin_game_name != cue_game_name:
-        print(f'.cue and .bin names in {filepath} don\`t match!')
-        continue
+        bin = find_pattern('*.bin', filepath)
+        cue = find_pattern('*.cue', filepath)
 
-    chdman_cmd = subprocess.check_output(f'chdman.exe -i {cue} -o {cue_game_name}.chd'.split(' '))
+        # skip files that aren't a dir and hidden files
+        if not os.path.isdir(filepath) or filepath.startswith('.'):
+            continue
 
-    # make sure we succeeded
-    chd = find_pattern('*.chd', filepath)
-    if not chd:
-        print(f'.chd not created! errors: {chdman_cmd}')
-    #TODO: find out if we can zip chds
+        # error handling
+        if not cue or not bin:
+            print(f'.cue and .bin not found in {filepath}!')
+            continue
+        else:
+            cue_game_name = cue.split('.cue')[0]
+        if find_pattern('*.chd', filepath):
+            print(f'{filepath}.chd already exists!')
+            continue
+
+        # execute chdman
+        try:
+            chdman_cmd = subprocess.check_output(
+                ['chdman.exe', 'createcd', '-i', cue, '-o', f'{cue_game_name}.chd'], stderr=subprocess.STDOUT
+            )
+        except subprocess.CalledProcessError as cpe:
+            print(cpe.output)
+
+        # make sure we succeeded
+        chd = find_pattern('*.chd', filepath)
+        if not chd:
+            print(f'.chd not created! errors: {chdman_cmd}')
+        else:
+            print(chdman_cmd)
+        #TODO: find out if we can zip chds
+
+
+if __name__ == '__main__':
+    if not os.path.exists('chdman.exe'):
+        raise FileNotFoundError('chdman.exe not found!')
+    convert_chds()
