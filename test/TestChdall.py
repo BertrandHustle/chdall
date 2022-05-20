465a58cc983eb194814c6e843cdbc2f0437701ea
BertrandHustle <scott.greenberg926@gmail.com>
2022-05-19 21:23:11 -0400

    tests refactored for unittest and pytest imports removed

--- test/TestChdall.py
+++ test/TestChdall.py
@@ -1,10 +1,43 @@
+from shutil import copytree, rmtree
+import os
+import unittest
+
 from chdall import *
+from paths import PROJECT_ROOT
+
+test_bin_cue_dirs = ['Ace_Combat_2__USA_', 'Advanced_V.G._2__Japan_']
 
 
-def test_move_chds():
-    create_chds()
-    move_chds()
-    parent_dir_chds = [f for f in os.listdir() if f.endswith('.chd')]
-    assert len(parent_dir_chds) == 2
+class TestChdAll(unittest.TestCase):
+    def setUp(self):
+        os.chdir('test_dir')
+        self._remove_test_dirs_and_chds()
+        for d in test_bin_cue_dirs:
+            copytree(
+                os.path.join(PROJECT_ROOT, d),
+                os.path.join(os.getcwd(), d)
+            )
 
+    def tearDown(self):
+        self._remove_test_dirs_and_chds()
+        os.chdir('..')
 
+    @staticmethod
+    def _remove_test_dirs_and_chds():
+        for f in os.listdir():
+            if f in test_bin_cue_dirs:
+                rmtree(f)
+            elif f.endswith('.chd'):
+                os.remove(f)
+
+    def create_chds(self):
+        pass
+
+    def test_move_chds(self):
+        create_chds()
+        move_chds()
+        parent_dir_chds = [f for f in os.listdir() if f.endswith('.chd')]
+        self.assertTrue(len(parent_dir_chds) == 2)
+
+    def test_delete_chds(self):
+        pass
